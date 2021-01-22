74200fdd4e47b43919e88333e6516024f8d97a77
kutsenkoilya <kutsenkoilya@hotmail.com>
2021-01-22 18:34:12 +0500

    settings upd

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'ask','qa'
+    'ask','qa',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
