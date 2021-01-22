c68e9a137f1a8765f70db8eb2f72659712980978
kutsenkoilya <kutsenkoilya@hotmail.com>
2021-01-22 13:10:28 +0500

    fixed sripts

--- hello.py
+++ hello.py
@@ -1,14 +1,10 @@
 from wsgiref.simple_server import make_server
-from cgi import parse_qs, escape
 
 def application(environ, start_response):
 	status = '200 OK'
 	headers = [
 		('Content-Type', 'text/plain')
 	]
-	body = ''
-	d = parse_qs(environ['QUERY_STRING'])
-	for key in d:
-		body += key+'='+d[key]+'\n'
+	body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
 	start_response(status, headers)
-	return [ body ]
+	return body
--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+cd /home/box/web
 gunicorn --bind='0.0.0.0:8080' hello:application
