461c96b5ecb90dfce8ff9635e25c05feba73fc30
kutsenkoilya <kutsenkoilya@hotmail.com>
2021-01-22 18:32:10 +0500

    config upd

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'ask','qa'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-cd /home/box/web
-gunicorn --bind='0.0.0.0:8080' hello:application
 cd ask/ask
 gunicorn --bind='0.0.0.0:8000' ask.wsgi:application
+cd /home/box/web
+gunicorn --bind='0.0.0.0:8080' hello:application
