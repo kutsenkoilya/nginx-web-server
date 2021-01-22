d1c717e3a9e79b992623b92cd786d8fee844162b
kutsenkoilya <kutsenkoilya@hotmail.com>
2021-01-22 18:47:20 +0500

    url upd

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^$', test, name='test'),
     url(r'^login/$', test, name='test'),
     url(r'^signup/$', test, name='test'),
-    url(r'^question/', include('qa.urls')),
+    url(r'^question/\d+', test, name='test'),
     url(r'^ask/$', test, name='test'),
     url(r'^popular/$', test, name='test'),
 	url(r'^new/$', test, name='test')
--- ask/qa/urls.py
+++
@@ -1,7 +0,0 @@
-
-from django.conf.urls import url
-from views import test
-
-urlpatterns = [
-    url(r'^(\d)+$', test, name='test'),
-]
