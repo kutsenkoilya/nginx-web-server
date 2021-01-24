264956882e6ffab1696915a46fccc33d19ae1675
kutsenkoilya <kutsenkoilya@hotmail.com>
2021-01-24 22:21:48 +0500

    db init comment

---
+++ ask/qa/models.py
@@ -0,0 +1,26 @@
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+    def popular(self):
+        return self.order_by('-rating')
+
+
+class Question(models.Model):
+    object = QuestionManager()
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+    
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL)
+    
--- init.sh
+++ init.sh
@@ -4,3 +4,13 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gun_hello.py  /etc/gunicorn.d/gun_hello.py
 sudo ln -sf /home/box/web/etc/gun_task.py /etc/gunicorn.d/task.py
 sudo /etc/init.d/gunicorn restart
+
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database stepik_web;"
+mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
+
+sudo python /home/box/web/ask/manage.py syncdb
+
+
+
+
