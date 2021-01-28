76f03fe5aee505ab4ddd8d42f70d6cb88882ce24
kutsenkoilya <kutsenkoilya@hotmail.com>
2021-01-28 19:58:32 +0500

    superuser create rem

--- init.sh
+++ init.sh
@@ -10,4 +10,3 @@ mysql -uroot -e "create database stepik_web;"
 mysql -uroot -e "grant all privileges on stepik_web.* to 'box'@'localhost' with grant option;"
 sudo python3 /home/box/web/ask/manage.py makemigrations qa
 sudo python3 /home/box/web/ask/manage.py migrate
-sudo python3 /home/box/web/ask/manage.py createsuperuser
